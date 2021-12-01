eb3446181cab12dc59cd71370c9030a929888837
Craig Handley <craig.handley@clicktravel.com>
2021-12-01 16:06:28 +0000

    split out screens

---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const RestaurantInfo = () => {
+  return (
+    <View>
+      <Text>Restaurant Info</Text>
+    </View>
+  );
+};
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
+
+import { RestaurantInfo } from "../components/restaurant-info.component";
+
+export const RestaurantsScreen = () => {
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar
+          placeholder="Search"
+          value={searchQuery}
+          onChangeText={onChangeSearch}
+        />
+      </View>
+      <View style={styles.list}>
+        <RestaurantInfo />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "white",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
